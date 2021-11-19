5bc48086ad8c7b1859f0db83bedab2c181fdb8e6
LeVanHieu0509 <levanhieu2k1.dz@gmail.com>
2021-11-19 21:55:58 +0700

    Initial commit

---
+++ app/src/main/java/levanhieu/lvh/ungdungtratudien/MainActivity.java
@@ -0,0 +1,60 @@
+package levanhieu.lvh.ungdungtratudien;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                display(item.getItemId());
+                return true;
+            }
+        });
+    }
+    void display(int id){
+        Fragment fragment = null;
+        switch (id){
+            case R.id.mnuHome:
+                toolbar.setTitle("Home");
+                fragment = new HomeFragment();
+                break;
+            case R.id.mnuHistory:
+                toolbar.setTitle("History");
+                fragment = new HistoryFragment();
+                break;
+            case R.id.mnuVocabulary:
+                toolbar.setTitle("Vocabulary");
+                fragment = new VocabularyFragment();
+                break;
+            case R.id.mnuDiscovery:
+                toolbar.setTitle("Discovery");
+                fragment = new DiscoveryFragment();
+                break;
+        }
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content, fragment);
+        ft.commit();
+    }
+}
